c310f9f62a0d031b7691418578469dd6938de1a7
fredostark <fhtran@leafhatrobotics.com>
2026-05-19 23:12:39 +0200

    chore: delete all old subsystems for light rewrite

--- src/components/modals/wizard/theme.ts
+++
@@ -1,4 +0,0 @@
-import { TextAttributes } from "@opentui/core";
-
-export const BOLD = TextAttributes.BOLD;
-export const ITALIC = TextAttributes.ITALIC;
--- src/core/prompts/shared/index.ts
+++
@@ -1,2 +0,0 @@
-export { buildDirectoryTree, buildSoulMapAck, buildSoulMapUserMessage } from "./soul-map.js";
-export { TOOL_GUIDANCE_NO_MAP, TOOL_GUIDANCE_WITH_MAP } from "./tool-guidance.js";
